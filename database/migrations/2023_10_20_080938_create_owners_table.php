b5dbeb942c74b98cba22a6481bbff6bb36c9f988
Marcel Balint <marcelbalint363@gmail.com>
2023-10-20 11:56:28 +0300

    Edite nullable

--- database/migrations/2023_10_20_080938_create_owners_table.php
+++ database/migrations/2023_10_20_080938_create_owners_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('surname');
-            $table->string('email');
-            $table->string('phone');
-            $table->text('address');
+            $table->string('first_name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
